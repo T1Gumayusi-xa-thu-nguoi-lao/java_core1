a1a5be7fdd5e460e0e9333c499f2920a08abe1c6
T1Gumayusi-xa-thu-nguoi-lao <ngkhanhff@gmail.com>
2024-11-12 20:48:33 +0700

    baitapgiaotrinh

---
+++ baitapgiaotrinh/Main.java
@@ -0,0 +1,9 @@
+package baitapgiaotrinh.chuong2.chuong3;
+
+public class Main {
+    public static void main(String[] args) {
+    Hang hang = new Hang() ;
+    hang.nhap();
+    hang.xuat ();
+    }
+}
